3ccbbf27566abb602d667309152b142abc718a70
Sergey <cerega32@mail.ru>
2023-04-05 06:59:15 +0300

    build: настроить конфиги для сборки

--- commitizen.js
+++ commitizen.js
@@ -1,18 +1,28 @@
-'use strict';
-
 module.exports = {
   // Добавим описание на русском языке ко всем типам
   types: [
-    {value: 'feat', name: 'feat:      Добавление нового функционала'},
-    {value: 'fix', name: 'fix:       Исправление ошибок'},
-    {value: 'design', name: 'design:     Добавление дизайна или изменение'},
-    {value: 'build', name: 'build:     Сборка проекта или изменения внешних зависимостей'},
-    {value: 'perf', name: 'perf:      Изменения направленные на улучшение производительности'},
-    {value: 'refactor', name: 'refactor:  Правки кода без исправления ошибок или добавления новых функций'},
-    {value: 'style', name: 'style:     Правки по кодстайлу (табы, отступы, точки, запятые и т.д.)'},
-    {value: 'docs', name: 'docs:      Обновление документации'},
-    {value: 'test', name: 'test:      Добавление тестов'},
-    {value: 'revert', name: 'revert:    Откат на предыдущие коммиты'},
+    { value: "feat", name: "feat:      Добавление нового функционала" },
+    { value: "fix", name: "fix:       Исправление ошибок" },
+    { value: "design", name: "design:     Добавление дизайна или изменение" },
+    {
+      value: "build",
+      name: "build:     Сборка проекта или изменения внешних зависимостей",
+    },
+    {
+      value: "perf",
+      name: "perf:      Изменения направленные на улучшение производительности",
+    },
+    {
+      value: "refactor",
+      name: "refactor:  Правки кода без исправления ошибок или добавления новых функций",
+    },
+    {
+      value: "style",
+      name: "style:     Правки по кодстайлу (табы, отступы, точки, запятые и т.д.)",
+    },
+    { value: "docs", name: "docs:      Обновление документации" },
+    { value: "test", name: "test:      Добавление тестов" },
+    { value: "revert", name: "revert:    Откат на предыдущие коммиты" },
   ],
 
   // Возможность задать спец ОБЛАСТЬ для определенного типа коммита (пример для 'fix')
@@ -28,26 +38,31 @@ module.exports = {
 
   // Поменяем дефолтные вопросы
   messages: {
-    type: 'Какие изменения вы вносите?',
+    type: "Какие изменения вы вносите?",
     // scope: "\nВыберите ОБЛАСТЬ, которую вы изменили (опционально):",
     // // Спросим если allowCustomScopes в true
     // customScope: "Укажите свою ОБЛАСТЬ:",
-    subject: 'Напишите КОРОТКОЕ описание в ПОВЕЛИТЕЛЬНОМ наклонении:\n',
+    subject: "Напишите КОРОТКОЕ описание в ПОВЕЛИТЕЛЬНОМ наклонении:\n",
     // body: 'Напишите ПОДРОБНОЕ описание (опционально). Используйте "|" для новой строки:\n',
     // breaking: "Список BREAKING CHANGES (опционально):\n",
     // footer: "Место для мета данных (тикетов, ссылок и остального). Например: SECRETMRKT-700, SECRETMRKT-800:\n",
-    confirmCommit: 'Вас устраивает получившийся коммит?',
+    confirmCommit: "Вас устраивает получившийся коммит?",
   },
 
   // Разрешим собственную ОБЛАСТЬ
-  allowCustomScopes: false,
+  allowCustomScopes: true,
 
   // Запрет на Breaking Changes
-  // allowBreakingChanges: false,
+  allowBreakingChanges: false,
 
   // Префикс для нижнего колонтитула
   // footerPrefix: "МЕТА ДАННЫЕ:",
 
   // limit subject length
   subjectLimit: 72,
+
+  typePrefix: "",
+  typeSuffix: "",
+
+  skipQuestions: ["body", "scope", "footer"],
 };
