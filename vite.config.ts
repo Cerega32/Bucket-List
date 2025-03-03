2d6322ba4668331b80e673f6d47366eb223da61d
Cerega32 <Cerega32@mail.ru>
2025-03-03 22:48:51 +0300

    build: изменен base в vite

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({mode}) => {
 
 	return {
 		plugins: [react(), legacy()],
-		base: './',
+		base: '/',
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
