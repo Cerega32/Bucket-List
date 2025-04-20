96b6bcd485de11c0366dba04b3fb832b2f496244
Cerega32 <Cerega32@mail.ru>
2025-04-20 15:18:04 +0300

    fix: поправлена регистрация и логин

--- src/pages/PageLogin/PageLogin.tsx
+++ src/pages/PageLogin/PageLogin.tsx
@@ -1,14 +1,26 @@
+import Cookies from 'js-cookie';
 import {FC, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import {Login} from '@/components/Login/Login';
 import {ThemeStore} from '@/store/ThemeStore';
+import {UserStore} from '@/store/UserStore';
 import {IPage} from '@/typings/page';
 
 export const PageLogin: FC<IPage> = ({page}) => {
 	const {setHeader, setPage, setFull} = ThemeStore;
 
+	const {setName, setIsAuth, setAvatar} = UserStore;
+
 	const navigate = useNavigate();
+
+	const successLogin = (data: {name: string}) => {
+		Cookies.set('name', data.name || '');
+		setName(data.name || '');
+		setIsAuth(true);
+		setAvatar(Cookies.get('avatar') || '');
+		navigate('/list/100-goals');
+	};
 
 	useEffect(() => {
 		setHeader('white');
@@ -17,5 +29,5 @@ export const PageLogin: FC<IPage> = ({page}) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return <Login isPage openRegistration={() => navigate('/sign-up')} successLogin={() => navigate('/')} />;
+	return <Login isPage openRegistration={() => navigate('/sign-up')} successLogin={successLogin} />;
 };
--- src/pages/PageRegistration/PageRegistration.tsx
+++ src/pages/PageRegistration/PageRegistration.tsx
@@ -1,14 +1,26 @@
+import Cookies from 'js-cookie';
 import {FC, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import {Registration} from '@/components/Registration/Registration';
 import {ThemeStore} from '@/store/ThemeStore';
+import {UserStore} from '@/store/UserStore';
 import {IPage} from '@/typings/page';
 
 export const PageRegistration: FC<IPage> = ({page}) => {
 	const {setHeader, setPage, setFull} = ThemeStore;
 
+	const {setName, setIsAuth, setAvatar} = UserStore;
+
 	const navigate = useNavigate();
+
+	const successRegistration = (data: {name: string}) => {
+		Cookies.set('name', data.name || '');
+		setName(data.name || '');
+		setIsAuth(true);
+		setAvatar(Cookies.get('avatar') || '');
+		navigate('/list/100-goals');
+	};
 
 	useEffect(() => {
 		setHeader('white');
@@ -17,5 +29,5 @@ export const PageRegistration: FC<IPage> = ({page}) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return <Registration isPage openLogin={() => navigate('/sign-in')} successRegistration={() => navigate('/')} />;
+	return <Registration isPage openLogin={() => navigate('/sign-in')} successRegistration={successRegistration} />;
 };
